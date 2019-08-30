f425f43b68754f41a2dc23f524334c56654625f6
Jacob Fenwick <jacobfenwick111@gmail.com>
2019-08-29 21:41:19 -0400

    pushed info into database and called database to append info the the screen when child_added

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,6 +1,8 @@
 $(document).ready(function() {
       firebase.initializeApp(firebaseConfig);
       
+      var database = firebase.database();
+
       var trCount = 0;
 
       $("button").on("click", function(event) {
@@ -12,16 +14,51 @@ $(document).ready(function() {
         var formDestination = $("#formDestination").val().trim();
         var trainTime = $("#trainTime").val().trim();
         var formFrequency = $("#formFrequency").val().trim();
-        console.log(trainName);
-        console.log(formDestination);
-        console.log(trainTime);
-        console.log(formFrequency);
 
-        var newTR = $("<tr class='" + trCount + "'>")
-        $("tbody").append($(newTR));
-        for(var i = 0; i < 5; i++) {
-            newTR.append($("<td>"))
-        };
+        var newTrain = {
+            train: trainName,
+            destination: formDestination,
+            time: trainTime,
+            frequency: formFrequency
+        }
 
+        database.ref().push(newTrain);
+        
+        console.log(newTrain.train);
+        console.log(newTrain.destination);
+        console.log(newTrain.time);
+        console.log(newTrain.frequency);
+
+        $("#trainName").val("");
+        $("#formDestination").val("");
+        $("#trainTime").val("");
+        $("#formFrequency").val("");
+      })
+
+      database.ref().on("child_added", function(childSnapshot){
+        console.log(childSnapshot);
+
+        var trainName = childSnapshot.val().train;
+        var formDestination = childSnapshot.val().destination;
+        var trainTime = childSnapshot.val().time;
+        var formFrequency = childSnapshot.val().frequency;
+
+        console.log(trainName)
+        console.log(formDestination)
+        console.log(trainTime)
+        console.log(formFrequency)
+
+        // var currentTime = ;
+        // var minAway = ;
+
+        var newRow = $("<tr>").append(
+            $("<td>").text(trainName),
+            $("<td>").text(formDestination),
+            $("<td>").text(formFrequency),
+            $("<td>").text(""),
+            $("<td>").text("")
+        );
+
+        $("tbody").append(newRow);
       })
 }) 
