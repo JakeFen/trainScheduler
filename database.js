c42dcf8950e998e566a8b4666616b8e47a2f9be6
Jacob Fenwick <jacobfenwick111@gmail.com>
2019-09-04 18:58:44 -0400

    delete .gitignore so the api would push

---
+++ database.js
@@ -0,0 +1,9 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyD0BL-vzbmNhUL5A0qpQ5udE-Vd6Nhzxjs",
+  authDomain: "trainscheduler-64ee7.firebaseapp.com",
+  databaseURL: "https://trainscheduler-64ee7.firebaseio.com",
+  projectId: "trainscheduler-64ee7",
+  storageBucket: "",
+  messagingSenderId: "786925156466",
+  appId: "1:786925156466:web:e2f84db3df46fc3d"
+};
